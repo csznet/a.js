f9a41c7dddd8cf82979a7af949b3a609d410f374
axww <axww@github.com>
2025-11-30 16:54:22 +0900

    SQL -u

--- src/uAdv.tsx
+++ src/uAdv.tsx
@@ -1,22 +1,19 @@
 import { Context } from "hono";
-import { Auth } from "./core";
+import { Auth, DB } from "./core";
 
 export async function uAdv(a: Context) {
     const i = await Auth(a)
     if (!i || i.grade < 2) { return a.text('401', 401) }
     const uid = parseInt(a.req.param('uid') ?? '0')
-    const user = (await DB(a)
-        .update(User)
-        .set({
-            grade: sql<number>`CASE WHEN ${User.grade} !=-1 THEN -1 ELSE 0 END`,
-        })
-        .where(and(
-            eq(User.uid, uid),
-            lt(User.grade, 1), // 无权封禁贵宾以上用户组
-        ))
-        .returning()
-    )?.[0]
     // 如果无法标记则报错
-    if (!user) { return a.text('410:gone', 410) }
+    if (!(await DB.db
+        .prepare(`
+            UPDATE user
+            SET grade = CASE WHEN grade != -1 THEN -1 ELSE 0 END
+            WHERE uid = ? AND grade < 1
+            RETURNING uid
+        `) // 无权封禁贵宾以上用户组
+        .get([uid])
+    )?.['uid']) { return a.text('410:gone', 410) }
     return a.text('ok')
 }
--- src/uBan.tsx
+++ src/uBan.tsx
@@ -1,58 +1,38 @@
 import { Context } from "hono";
-import { Auth } from "./core";
+import { Auth, DB } from "./core";
 
 export async function uBan(a: Context) {
     const i = await Auth(a)
     if (!i || i.grade < 2) { return a.text('401', 401) }
     const uid = parseInt(a.req.param('uid') ?? '0')
-    const user = (await DB(a)
-        .update(User)
-        .set({
-            grade: -2,
-        })
-        .where(and(
-            eq(User.uid, uid),
-            lt(User.grade, 1), // 无权封禁贵宾以上用户组
-        ))
-        .returning()
-    )?.[0]
     // 如果无法标记则报错
-    if (!user) { return a.text('410:gone', 410) }
-    // 找出所有违规者帖子
-    const topic = DB(a).$with('topic').as(
-        DB(a)
-            .select({ pid: Post.pid })
-            .from(Post)
-            .where(and(
-                inArray(Post.attr, [0, 1]),
-                eq(Post.user, uid),
-                gt(Post.land, 0), // 必须是Thread(land>0)
-            ))
-    )
-    // 删除违规者所有帖子和回复，以及他人的回复。with要在update之前，否则post改变后子查询失效。
-    await DB(a).batch([
-        DB(a)
-            .with(topic)
-            .update(Post)
-            .set({
-                attr: 1,
-            })
-            .where(and(
-                eq(Post.attr, 0),
-                inArray(Post.land, sql<number[]>`(SELECT -pid FROM ${topic})`), // 所有tid=pid=-land相反数
-                ne(Post.user, uid),
-            )) // 更新thread
-        ,
-        DB(a)
-            .update(Post)
-            .set({
-                attr: 3,
-            })
-            .where(and(
-                inArray(Post.attr, [0, 1]),
-                eq(Post.user, uid),
-            ))
-        ,
-    ])
+    if (!(await DB.db
+        .prepare(`
+            UPDATE user
+            SET grade = CASE WHEN grade != -2 THEN -2 ELSE 0 END
+            WHERE uid = ? AND grade < 1
+            RETURNING uid
+        `) // 无权封禁贵宾以上用户组
+        .get([uid])
+    )?.['uid']) { return a.text('410:gone', 410) }
+    // 删除违规者所有帖子 将所有回复设为隐藏
+    await DB.db
+        .prepare(`
+            WITH thread AS (
+                SELECT pid AS tid
+                FROM post
+                WHERE attr IN (0,1) AND user = ? AND land > 0
+            )
+            UPDATE post
+            SET attr = 1
+            WHERE attr = 0
+            AND land IN (SELECT -tid FROM thread)
+            AND user != ?
+        `) // 必须是thread(land>0) 回复land=-tid
+        .run([uid, uid])
+    // 隐藏完他人回复后 删除违规者所有帖子
+    await DB.db
+        .prepare(`UPDATE post SET attr = 3 WHERE attr IN (0,1) AND user = ?`)
+        .run([uid])
     return a.text('ok')
 }
