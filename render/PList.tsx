873182bbc3e84bffd5c51c2da56fd482ef07dec0
axww <114227169+axww@users.noreply.github.com>
2025-12-06 10:59:18 +0900

    Remove user link

--- render/PList.tsx
+++ render/PList.tsx
@@ -120,7 +120,7 @@ ${CBegin(a, z)}
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                             </svg>
-                            <a href="/?user=${item.user}&land=${z.data[0].land}" target="_blank" class="link link-hover truncate max-w-[120px] role_${item.grade}">${item.name}</a>
+                            <span class="truncate max-w-[120px] role_${item.grade}">${item.name}</span>
                         </div>
                         <span class="flex items-center gap-2 min-w-0">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
